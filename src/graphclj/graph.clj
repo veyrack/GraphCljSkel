83e4d63da843f14fd13b6f868ce6a77d71c69fcb
veyrack <veyrack.lin@etu.upmc.fr>
2019-03-22 20:59:54 +0100

    Partie 1

--- src/graphclj/graph.clj
+++ src/graphclj/graph.clj
@@ -1,16 +1,43 @@
 (ns graphclj.graph
   (:require [clojure.string :as str]))
 
+
+(defn add-graph [graph edge]
+  (let [[s1 s2] (map #(Integer/parseInt %) (str/split edge #" "))]
+    (let [info (if (contains? graph s1)
+                 {:neigh (conj (:neigh (get graph s1)) s2)}
+                 {:neigh #{s2}})]
+      (assoc graph s1 info))))
+
+(add-graph {} "1 2")
+;{1 {:neigh #{2}}}
+(add-graph {1 {:neigh #{2}}} "2 1")
+;{1 {:neigh #{2}}, 2 {:neigh #{1}}}
+
 ;; Generate a graph from the lines
 (defn gen-graph [lines]
     "Returns a hashmap contating the graph"
-    (loop [s lines,res {}]
-      (let [f (first (first s)),r (rest (first s))]
-        (if (contains? res f)
-          (assoc res f (clojure.set/union (get res f) r))))))
+    (let [inverse (map (fn [x] (apply str (reverse x))) lines)]
+      (reduce add-graph {} (concat lines inverse))))
+
+(gen-graph '("1 2" "2 1" "3 1"))
+;{1 {:neigh #{3 2}}, 2 {:neigh #{1}}, 3 {:neigh #{1}}}
 
 
-
+(defn transfo-pair [coll]
+  (if (not (empty? coll))
+    (concat
+      (map (fn [x] #{(first coll) x}) (rest coll))
+      (transfo-pair (rest coll)))
+    ()))
 
 (defn erdos-renyi-rnd [n,p]
-  "Returns a G_{n,p} random graph, also known as an Erdős-Rényi graph")
+  "Returns a G_{n,p} random graph, also known as an Erdős-Rényi graph"
+  (let [liste (take n (range))
+        pair (transfo-pair liste)
+        lines (filter (fn [x] (< (rand) p)) pair)
+        lines (mapcat (fn [x] [(str (first x) " " (second x))]) lines)]
+    (gen-graph lines)))
+
+(erdos-renyi-rnd 5 0.5)
+;{0 {:neigh #{4 2}}, 1 {:neigh #{4 2}}, 4 {:neigh #{0 1 2}}, 2 {:neigh #{0 1 4}}}
