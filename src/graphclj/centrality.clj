69a7ca7e3380ac637b8c58c7c3ffdd35bc75c30f
veyrack <veyrack.lin@etu.upmc.fr>
2019-03-24 19:24:59 +0100

    Miss main

--- src/graphclj/centrality.clj
+++ src/graphclj/centrality.clj
@@ -5,15 +5,82 @@
 
 
 (defn degrees [g]
-  "Calculates the degree centrality for each node")
+  "Calculates the degree centrality for each node"
+  (into {} (map (fn [x] {(first x) (merge (second x) {:degree (count (get (second x) :neigh))})}) g)))
 
+(degrees {1 {:neigh #{2 3}},0 {:neigh #{1 2}}})
+;{1 {:neigh #{3 2}, :degree 2}, 0 {:neigh #{1 2}, :degree 2}}
 
 (defn distance [g n]
-  "Calculate the distances of one node to all the others")
+  "Calculate the distances of one node to all the others"
+  (let [node (get-nodes g)]
+    (let [actual (get-ens (get g n))]
+      (loop [node node,actual actual,res {}]
+        (if (seq node)
+          (recur (rest node) actual (dist g res (first node) actual n 1.0))
+          res)))))
+
+(defn dist [g res node actual n cpt]
+  (if (= node n)
+    (assoc res node 0.0)
+    (if (contains? res node)
+      ()
+      (if (contains? actual node)
+        (assoc res node cpt)
+        (recur g res node (new-actual g actual) n (inc cpt))))))
+
+(defn new-actual [g actual]
+  (loop [actual actual,res #{}]
+    (if (seq actual)
+      (recur (rest actual) (clojure.set/union res (get (get g (first actual)) :neigh)))
+      res)))
+
+
+(let
+  [g {1 {:neigh #{0 4 3}},
+      0 {:neigh #{1 3}},
+      3 {:neigh #{0 1 2}},
+      4 {:neigh #{1}},
+      2 {:neigh #{3}}}]
+  (distance g 0))
+;{1 0.0, 0 1.0, 3 1.0, 4 1.0, 2 2.0}
+
+(defn get-ens [node]
+  (get node :neigh))
+
+(get-ens {1 {:neigh #{0 4 3}}})
+
+(defn get-nodes [g]
+  (map (fn [x] (first x)) g))
+
+(get-nodes {1 {:neigh #{0 4 3}}, 0 {:neigh #{1 3}}})
+
 
 (defn closeness [g n]
-  "Returns the closeness for node n in graph g")
+  "Returns the closeness for node n in graph g"
+  (let [dist (distance g n)]
+    (reduce + (map (fn [x] (if (= (second x) 0.0)
+                             (second x)
+                             (/ 1.0 (second x))))
+                  dist))))
 
+(let
+  [g {1 {:neigh #{0 4 3}},
+      0 {:neigh #{1 3}},
+      3 {:neigh #{0 1 2}},
+      4 {:neigh #{1}},
+      2 {:neigh #{3}}}]
+  (closeness g 0))
+;3.5
 
 (defn closeness-all [g]
-  "Returns the closeness for all nodes in graph g")
+  "Returns the closeness for all nodes in graph g"
+  (into {} (map (fn [x] {(first x) (merge (second x) {:close (closeness g (first x))})}) g)))
+
+(let
+  [g {1 {:neigh #{0 4 3}},
+      0 {:neigh #{1 3}},
+      3 {:neigh #{0 1 2}},
+      4 {:neigh #{1}},
+      2 {:neigh #{3}}}]
+  (closeness-all g))
--- src/graphclj/tools.clj
+++ src/graphclj/tools.clj
@@ -7,7 +7,39 @@
             (doall (line-seq rdr))))
 
 (defn rank-nodes [g,l]
-  "Ranks the nodes of the graph in relation to label l in accending order")
+  "Ranks the nodes of the graph in relation to label l in accending order"
+  (into {} (map (fn [x] {(first x) (merge (second x) {:rank (get-rank g l (get (second x) l))})}) g)))
+
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (rank-nodes g :close))
+;{1 {:neigh #{0 4 3}, :close 4.0, :rank 3},
+; 0 {:neigh #{1 3}, :close 3.5, :rank 2},
+; 3 {:neigh #{0 1 2}, :close 4.0, :rank 3},
+; 4 {:neigh #{1}, :close 2.8, :rank 0},
+; 2 {:neigh #{3}, :close 2.8, :rank 0}
+
+(defn get-rank [g l node]
+  (loop [s g,res 0]
+    (if (seq s)
+      (if (< (get (second (first s)) l) node)
+        (recur (rest s) (inc res))
+        (recur (rest s) res))
+      res)))
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (get-rank g :close 2.8))
+
 
 
 (defn generate-colors [n]
@@ -22,4 +54,97 @@
 
 
 (defn to-dot [g]
-  "Returns a string in dot format for graph g, each node is colored in relation to its ranking")
+  "Returns a string in dot format for graph g, each node is colored in relation to its ranking"
+  (let [colors (generate-colors (count g))
+        node (print-node g colors)
+        link (print-link g)
+        res (str "graph g{\n"node link"}")]
+    (do (println res) res)))
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (to-dot g))
+
+
+(defn print-node [g colors]
+  (loop [s g,res ""]
+    (if (seq s)
+      (recur (rest s) (str res (first (first s))" [style=filled color="(pr-str (str/join " " (get colors (first (first s)))))"]\n"))
+      res)))
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (print-node g (generate-colors 5)))
+
+(defn print-link [g]
+  (let [link (delete-doublon (get-all-link g))]
+    (loop [s link,res ""]
+      (if (seq s)
+        (recur (rest s) (str res (first s)"\n"))
+        res))))
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (print-link g))
+
+
+(defn get-all-link [g]
+  (loop [s g,res ""]
+    (if (seq s)
+      (recur (rest s) (str res (get-link (first (first s)) (get (second (first s)) :neigh))))
+      (str/split res #" "))))
+
+(defn get-link [node link]
+  (loop [s link,res ""]
+    (if (seq s)
+      (recur (rest s) (str res (str node"--" (first s)" ")))
+      res)))
+
+(get-link 1 #{0 3 4})
+;"1--0 1--4 1--3 "
+
+(let
+  [g {1 {:neigh #{0 4 3}, :close 4.0},
+      0 {:neigh #{1 3}, :close 3.5 },
+      3 {:neigh #{0 1 2}, :close 4.0},
+      4 {:neigh #{1}, :close 2.8},
+      2 {:neigh #{3}, :close 2.8}}]
+  (get-all-link g))
+;["1--0" "1--4" "1--3" "0--1" "0--3" "3--0" "3--1" "3--2" "4--1" "2--3"]
+
+
+(defn delete-doublon [entry]
+  (loop [s entry,res entry]
+    (if (seq s)
+      (let [s1 (first (str/split (first s) #"--")) s2 (second (str/split (first s) #"--")) ss (str s2"--"s1)]
+        (if (contains-str? res ss)
+          (recur (rest s) (remove #{(first s)} res))
+          (recur (rest s) res)))
+      res)))
+
+
+(delete-doublon ["1--0" "1--4" "1--3" "0--1" "0--3" "3--0" "3--1" "3--2" "4--1" "2--3"])
+;("0--1" "3--0" "3--1" "4--1" "2--3")
+
+(defn contains-str? [vec str]
+  (loop [s vec]
+    (if (seq s)
+      (if (= (first s) str)
+        true
+        (recur (rest s)))
+      false)))
+
+(contains-str? ["1--0" "0--1"] "1--0")
+;true
